b2f622148182435fba7b490a241ff7d72dbb9ba2
hanifkhair17 <126538122+hanifkhair@users.noreply.github.com>
2023-04-05 08:52:40 +0700

    Update Navbar Playbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,58 +1,66 @@
-import "../css/stylesNavbar.css";
 import { Box, Button, ButtonGroup, IconButton } from "@chakra-ui/react";
-import { TfiArrowCircleLeft } from "react-icons/tfi";
-import { TfiArrowCircleRight } from "react-icons/tfi";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 export default function Navbar() {
-  return (
-    <>
-      <Box
-        width={"100vw"}
-        paddingLeft={"261px"}
-        paddingRight={"20px"}
-        alignItems={"center"}
-        top={0}
-        pos="fixed"
-        height={"64px"}
-        display={"flex"}
-        justifyContent={"space-between"}
-        bgColor={"black"}
-        zIndex={"1"}
-      >
-        <Box>
-          <ButtonGroup color={"black"}>
-            <IconButton
-              as={TfiArrowCircleLeft}
-              bgColor={"black"}
-              color={"white"}
-            ></IconButton>
-            <IconButton
-              as={TfiArrowCircleRight}
-              bgColor={"black"}
-              color={"white"}
-            ></IconButton>
-          </ButtonGroup>
-        </Box>
-        <Box>
-          <ButtonGroup>
-            <Button
-              color={"#A7A7A7"}
-              font={"16px"}
-              padding={"8px 32px 8px 8px"}
-            >
-              Sign Up
-            </Button>
-            <Button
-              color={"#000000"}
-              font={"16px"}
-              bgColor={"#FFFFFF"}
-              padding={"8px 32px"}
-            >
-              Log In
-            </Button>
-          </ButtonGroup>
-        </Box>
-      </Box>
-    </>
-  );
+	return (
+		<>
+			<Box
+				width={"100vw"}
+				paddingLeft={"261px"}
+				paddingRight={"20px"}
+				alignItems={"center"}
+				top={0}
+				pos="fixed"
+				height={"64px"}
+				display={"flex"}
+				justifyContent={"space-between"}
+				bgColor={"black"}
+				zIndex={"1"}
+			>
+				<Box>
+					<ButtonGroup color={"black"}>
+						<IconButton
+							as={MdOutlineKeyboardArrowLeft}
+							bgColor={"black"}
+							color={"#A0A0A0"}
+							_hover={{ background: "black" }}
+							cursor={"pointer"}
+						></IconButton>
+						<IconButton
+							as={MdOutlineKeyboardArrowRight}
+							bgColor={"black"}
+							color={"#A0A0A0"}
+							_hover={{ background: "black" }}
+							cursor={"pointer"}
+						></IconButton>
+					</ButtonGroup>
+				</Box>
+				<Box>
+					<ButtonGroup>
+						<Button
+							color={"#A7A7A7"}
+							font={"16px"}
+							padding={"8px 32px 8px 8px"}
+							borderRadius={"25px"}
+							bgColor={"black"}
+							_hover={{ background: "black" }}
+						>
+							Sign Up
+						</Button>
+						<Button
+							color={"#000000"}
+							font={"16px"}
+							bgColor={"#FFFFFF"}
+							padding={"8px 32px"}
+							borderRadius={"25px"}
+						>
+							Log In
+						</Button>
+						<Button>Profile</Button>
+					</ButtonGroup>
+				</Box>
+			</Box>
+		</>
+	);
 }
--- src/components/playbar.jsx
+++ src/components/playbar.jsx
@@ -1,73 +1,175 @@
-import { position } from "@chakra-ui/react";
-import "../css/stylesPlaybar.css";
-import "../images/bg-spotify.jpg";
-//import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css";
-
-// import {
-// 	Box,
-// 	Button,
-// 	ButtonGroup,
-// 	IconButton,
-// 	Container,
-// 	Flex,
-// 	Image,
-// 	Icon,
-// 	Text,
-// 	Stack,
-// } from "@chakra-ui/react";
+import {
+	Box,
+	Container,
+	Flex,
+	IconButton,
+	Image,
+	Link,
+	Icon,
+	Slider,
+	SliderTrack,
+	SliderFilledTrack,
+	SliderThumb,
+	Center,
+} from "@chakra-ui/react";
+import {
+	BiSkipNext,
+	BiShuffle,
+	BiSkipPrevious,
+	BiRepeat,
+} from "react-icons/bi";
+import { GoPlay } from "react-icons/go";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BsPip } from "react-icons/bs";
+import { TbMicrophone2, TbDevices2 } from "react-icons/tb";
+import { HiOutlineQueueList } from "react-icons/hi2";
+import { SlVolume2 } from "react-icons/sl";
+import pic from "../images/bg-spotify.jpg";
 
 export default function Playbar() {
-  return (
-    <>
-      <div
-        class="music-player"
-        style={{ bottom: "0", position: "fixed", width: "100%", zIndex: "3" }}
-      >
-        <div class="song-bar">
-          <div class="song-infos">
-            <div class="image-container">
-              <img
-                src="https://images.unsplash.com/photo-1562442507-cfa3c80f9c03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1126&q=80"
-                alt="Ramadhan nasheed"
-              />
-            </div>
-            <div class="song-description">
-              <p class="title">Ramadhan</p>
-              <p class="artist">Mishari Rashid</p>
-            </div>
-          </div>
-          <div class="icons">
-            <i class="far fa-heart"></i>
-            <i class="fas fa-compress"></i>
-          </div>
-        </div>
-        <div class="progress-controller">
-          <div class="control-buttons">
-            <i class="fas fa-random"></i>
-            <i class="fas fa-step-backward"></i>
-            <i class="play-pause fas fa-play"></i>
-            <i class="fas fa-step-forward"></i>
-            <i class="fas fa-undo-alt"></i>
-          </div>
-          <div class="progress-container">
-            <span>0:49</span>
-            <div class="progress-bar">
-              <div class="progress"></div>
-            </div>
-            <span>3:15</span>
-          </div>
-        </div>
-        <div class="other-features">
-          <i class="fas fa-list-ul"></i>
-          <i class="fas fa-desktop"></i>
-          <div class="volume-bar">
-            <i class="fas fa-volume-down"></i>
-            <div class="progress-bar">
-              <div class="progress"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<Container bottom="0" position="fixed" width="100%" zIndex="3">
+			<Flex
+				className="music-container"
+				bg="#111"
+				w="100%"
+				h={112}
+				position={"fixed"}
+				bottom={0}
+				direction="row"
+				align="center"
+				justify="center"
+			>
+				{/* Description */}
+				<Flex className="msc-desc" gap={3} w={"30%"} align={"center"}>
+					<Image src={pic} w={70} h={70} />
+					<Flex className="msc-intr" direction={"column"} justify={"center"}>
+						<Link href="#" color={"white"}>
+							Ramadhan
+						</Link>
+						<Link href="#" color={"white"}>
+							Mishari Rashid
+						</Link>
+					</Flex>
+					<Link href="#">
+						<Icon as={AiOutlineHeart} w={5} h={5} color={"white"}></Icon>
+					</Link>
+					<Link href="#">
+						<Icon as={BsPip} w={4} h={4} color={"white"}></Icon>
+					</Link>
+				</Flex>
+
+				{/* Player Button */}
+				<Flex
+					className="msc-controller"
+					direction="column"
+					align="center"
+					justify="center"
+					width={"30%"}
+				>
+					<Flex
+						className="ctrl-button"
+						align={"center"}
+						width={"100%"}
+						justify={"center"}
+						gap={3}
+					>
+						<Box>
+							<IconButton
+								variant={"link"}
+								as={BiShuffle}
+								style={{ width: "20px", height: "20px" }}
+								color="#999"
+							></IconButton>
+						</Box>
+						<Box>
+							<IconButton
+								variant={"link"}
+								as={BiSkipPrevious}
+								style={{ width: "40px", height: "40px" }}
+								color="#999"
+							></IconButton>
+						</Box>
+
+						<IconButton
+							variant={"link"}
+							as={GoPlay}
+							style={{ width: "40px", height: "40px" }}
+							color="white"
+						></IconButton>
+
+						<Box>
+							<IconButton
+								variant={"link"}
+								as={BiSkipNext}
+								style={{ width: "40px", height: "40px" }}
+								color="#999"
+							></IconButton>
+						</Box>
+						<Box>
+							<IconButton
+								variant={"link"}
+								as={BiRepeat}
+								style={{ width: "20px", height: "20px" }}
+								color="#999"
+							></IconButton>
+						</Box>
+					</Flex>
+					<Box height={1} width={"100%"}></Box>
+					<Flex
+						className="prog-pool"
+						align={"center"}
+						justify={"space-between"}
+						width={"100%"}
+					>
+						<Box color={"white"} width={10}>
+							<Center>0:00</Center>
+						</Box>
+
+						<Box className="prog-bar" width={"100%"}>
+							<Slider aria-label="slider-ex-1" w={350}>
+								<SliderTrack>
+									<SliderFilledTrack />
+								</SliderTrack>
+								<SliderThumb />
+							</Slider>
+						</Box>
+
+						<Box color={"white"} width={10}>
+							<Center>3:20</Center>
+						</Box>
+					</Flex>
+				</Flex>
+
+				{/* Control */}
+				<Flex
+					className="msc-opt"
+					w={"30%"}
+					justify={"right"}
+					gap={2}
+					align={"center"}
+					h={50}
+				>
+					<Link>
+						<Icon as={TbMicrophone2} w={6} h={5} color={"white"}></Icon>
+					</Link>
+					<Link>
+						<Icon as={HiOutlineQueueList} w={6} h={5} color={"white"}></Icon>
+					</Link>
+					<Link>
+						<Icon as={TbDevices2} w={6} h={5} color={"white"}></Icon>
+					</Link>
+					<Link>
+						<Icon as={SlVolume2} w={6} h={5} color={"white"}></Icon>
+					</Link>
+					<Slider aria-label="slider-ex-1" w={28}>
+						<SliderTrack>
+							<SliderFilledTrack />
+						</SliderTrack>
+						<SliderThumb />
+					</Slider>
+				</Flex>
+			</Flex>
+		</Container>
+	);
 }
